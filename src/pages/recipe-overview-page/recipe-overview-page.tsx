fff999fe92a16eede3d7c10554ffa3b77db0dcb1
Kim <kim.bertrand@live.com>
2021-02-16 21:58:36 +0100

    Made an example thingy to play with the styles

--- src/pages/recipe-overview-page/recipe-overview-page.tsx
+++ src/pages/recipe-overview-page/recipe-overview-page.tsx
@@ -1,9 +1,29 @@
 import React from "react";
+import '../recipe-overview-page/recipe-overview-page.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export default function RecipeOverviewPage() {
     return (
-        <div>
+        <div className="overview-page">
             Recipe Overview Page
+            <div className="recipe-tile">
+                <strong>Vegetarian Chickpea Curry</strong>
+                <div className="recipe-info-block">
+                    <div className="recipe-info">
+                        <p><FontAwesomeIcon icon={["fas", "male"]} /> - 4</p>
+                        <p><FontAwesomeIcon icon={["far", "clock"]} /> - 50 mins</p>
+                        <p><FontAwesomeIcon icon={["fas", "hamburger"]} /> - 500 kcal</p>
+                    </div>
+                    <div style={{maxWidth: "50%", maxHeight: "100%", padding: "0", margin: "0" }}>
+                        <img
+                            className="recipe-photo"
+                            src="https://www.eefkooktzo.nl/wp-content/uploads/2020/04/Indiase-curry-met-paneer.jpg"
+                            alt="curry"
+                            style={{maxHeight: "100%", maxWidth: "100%", objectFit: "contain"}}
+                        ></img>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
