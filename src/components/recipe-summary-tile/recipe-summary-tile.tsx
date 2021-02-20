905337452b71abb384970dfa8718425984f2d4bd
Kim <kim.bertrand@live.com>
2021-02-20 17:17:08 +0100

    Tweeked styling

--- src/components/recipe-summary-tile/recipe-summary-tile.tsx
+++ src/components/recipe-summary-tile/recipe-summary-tile.tsx
@@ -7,38 +7,42 @@ import {
   faMale,
 } from '@fortawesome/free-solid-svg-icons';
 
-export default function RecipeSummaryTile() {
+type props = {
+  recipeName: string;
+  portions: number;
+  prepTimeInMinutes: number;
+  kcalPerPortion: number;
+  imgUrl: string;
+};
+
+export default function RecipeSummaryTile({
+  recipeName,
+  portions,
+  prepTimeInMinutes,
+  kcalPerPortion,
+  imgUrl,
+}: props) {
   return (
     <div className="recipe-tile">
       <div className="recipe-title">
-        <strong>Vegetarian Chickpea Curry</strong>
+        <strong>{recipeName}</strong>
       </div>
       <div className="recipe-info-block">
         <div className="recipe-info">
           <div>
-            <FontAwesomeIcon icon={faMale} /> - 4
+            <FontAwesomeIcon icon={faMale} /> {portions} persons
           </div>
           <div>
-            <FontAwesomeIcon icon={faClock} /> - 50 mins
+            <FontAwesomeIcon icon={faClock} /> {prepTimeInMinutes} mins
           </div>
           <div>
-            <FontAwesomeIcon icon={faHamburger} /> - 500 kcal
+            <FontAwesomeIcon icon={faHamburger} /> {kcalPerPortion} kcal
           </div>
         </div>
         <div
-          style={{
-            maxWidth: '50%',
-            maxHeight: '100%',
-            padding: '0',
-            margin: '0',
-          }}
-        >
-          <img
-            className="recipe-photo"
-            src="https://www.eefkooktzo.nl/wp-content/uploads/2020/04/Indiase-curry-met-paneer.jpg"
-            alt="curry"
-          ></img>
-        </div>
+          className="recipe-photo"
+          style={{ backgroundImage: `url(${imgUrl}` }}
+        />
       </div>
     </div>
   );
--- src/pages/recipe-overview-page/recipe-overview-page.tsx
+++ src/pages/recipe-overview-page/recipe-overview-page.tsx
@@ -6,7 +6,27 @@ export default function RecipeOverviewPage() {
   return (
     <div className="overview-page">
       <h1>Recipes</h1>
-      <RecipeSummaryTile />
+      <RecipeSummaryTile
+        recipeName="Vegetarian Chickpea Curry"
+        portions={4}
+        prepTimeInMinutes={50}
+        kcalPerPortion={500}
+        imgUrl="https://www.eefkooktzo.nl/wp-content/uploads/2020/04/Indiase-curry-met-paneer.jpg"
+      />
+      <RecipeSummaryTile
+        recipeName="Brownies"
+        portions={6}
+        prepTimeInMinutes={60}
+        kcalPerPortion={250}
+        imgUrl="https://www.leukerecepten.nl/wp-content/uploads/2019/10/brownies.jpg"
+      />
+      <RecipeSummaryTile
+        recipeName="Bami met Saté"
+        portions={4}
+        prepTimeInMinutes={40}
+        kcalPerPortion={600}
+        imgUrl="https://www.ramadanrecepten.nl/wp-content/uploads/2015/05/bami-spiesjes-kip-satesaus-1.jpg"
+      />
     </div>
   );
 }
