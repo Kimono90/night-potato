6cd2d330dcb1906fbbd73e89dcc7ad13b859886e
Kim <kim.bertrand@live.com>
2021-02-21 09:36:53 +0100

    Added some more recipes for scrolling purposes

--- src/pages/recipe-overview-page/recipe-overview-page.tsx
+++ src/pages/recipe-overview-page/recipe-overview-page.tsx
@@ -11,21 +11,35 @@ export default function RecipeOverviewPage() {
         portions={4}
         prepTimeInMinutes={50}
         kcalPerPortion={500}
-        imgUrl="https://www.eefkooktzo.nl/wp-content/uploads/2020/04/Indiase-curry-met-paneer.jpg"
+        imgUrl="https://images.unsplash.com/photo-1585937421612-70a008356fbe"
       />
       <RecipeSummaryTile
         recipeName="Brownies"
         portions={6}
         prepTimeInMinutes={60}
         kcalPerPortion={250}
-        imgUrl="https://www.leukerecepten.nl/wp-content/uploads/2019/10/brownies.jpg"
+        imgUrl="https://images.unsplash.com/photo-1515037893149-de7f840978e2"
       />
       <RecipeSummaryTile
-        recipeName="Bami with Saté"
+        recipeName="Poke Bowl"
         portions={4}
         prepTimeInMinutes={40}
         kcalPerPortion={600}
-        imgUrl="https://www.ramadanrecepten.nl/wp-content/uploads/2015/05/bami-spiesjes-kip-satesaus-1.jpg"
+        imgUrl="https://images.unsplash.com/photo-1542354255-839e272e3408"
+      />
+      <RecipeSummaryTile
+        recipeName="Paõ de Queijo"
+        portions={2}
+        prepTimeInMinutes={45}
+        kcalPerPortion={350}
+        imgUrl="https://images.unsplash.com/photo-1598142982901-df6cec10ae35"
+      />
+      <RecipeSummaryTile
+        recipeName="Pizza Margherita"
+        portions={2}
+        prepTimeInMinutes={90}
+        kcalPerPortion={600}
+        imgUrl="https://images.unsplash.com/photo-1574071318508-1cdbab80d002"
       />
     </div>
   );
