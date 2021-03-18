6a904791d161e9f843c8c7778de61b40a8da620b
Kim <kim.bertrand@live.com>
2021-03-18 18:12:19 +0100

    Add dots below image gallery

--- src/components/recipe-photo-carousel/photo-carousel.styles.ts
+++ src/components/recipe-photo-carousel/photo-carousel.styles.ts
@@ -18,18 +18,13 @@ export const StyledCarouselPhoto = styled.div<CarouselPhotoProps>`
   height: 12rem;
   background-size: cover;
   border-radius: 0.5rem;
-  margin: 0 1rem 2rem 1rem;
+  margin: 0 1rem 0.5rem 1rem;
   padding: 0 2rem;
   background-image: ${props => props.imgUrl};
  opacity: 1;
- transition: opacity 0.3s ease-in-out;
  
- :hover {
-  opacity: 0.5;
- }
-
  @media(min-width: 500px) {
-    margin: 0 2rem 4rem 2rem;    
+    margin: 0 2rem 1rem 2rem;    
     width: 20rem;
     height: 20rem;
   }
@@ -37,7 +32,6 @@ export const StyledCarouselPhoto = styled.div<CarouselPhotoProps>`
 
 export const StyledArrowIcon = styled(FontAwesomeIcon)`
   font-size: 2rem;
-  margin-bottom: 2rem;
   transition: all 0.3s ease-in-out;
  
   :active {
@@ -52,4 +46,19 @@ export const StyledArrowIcon = styled(FontAwesomeIcon)`
       transform: ${props => props.icon === faChevronLeft ? 'translateX(-4px)' : 'translateX(4px)'};
     }
   }
+`
+
+type DotProps = {
+ active: boolean
+}
+
+export const StyledDot = styled.div<DotProps>`
+  cursor: pointer;
+  height: 10px;
+  width: 10px;
+  margin: 0 1rem 2rem 1rem;
+  background-color: ${props => props.active ? 'white' : 'grey'};;
+  border-radius: 50%;
+  display: inline-block;
+  transition: background-color 0.6s ease;
 `
