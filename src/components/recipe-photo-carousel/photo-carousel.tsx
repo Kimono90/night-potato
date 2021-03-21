70d9ba8fddfdefb716c6cf8e290db26fcb5c394c
Geraldo dos Santos <geraldo.santos@dephion.com>
2021-03-21 14:04:59 +0100

    More elegant css fix for default animation state

--- src/components/recipe-photo-carousel/photo-carousel.tsx
+++ src/components/recipe-photo-carousel/photo-carousel.tsx
@@ -18,13 +18,12 @@ type Props = {
 
 export default function PhotoCarousel({ imgUrls }: Props) {
   const [selectedImgIndex, setSelectedImgIndex] = useState<number>(0);
-  const selectedPhotoRef = useRef<HTMLDivElement>(null);
   const swipeHandlers = useSwipeable({
     onSwipedLeft: () => handleNextImgClick(),
     onSwipedRight: () => handlePreviousImgClick(),
     preventDefaultTouchmoveEvent: true,
-    trackMouse: true
-  })
+    trackMouse: true,
+  });
 
   const handleNextImgClick = () => {
     return selectedImgIndex === imgUrls.length - 1
@@ -48,14 +47,6 @@ export default function PhotoCarousel({ imgUrls }: Props) {
     ));
   };
 
-  useEffect(() => {
-    const { current: photo } = selectedPhotoRef;
-    if (photo) {
-      photo.removeAttribute('style');
-      photo.style.opacity = '1';
-    }
-  }, [selectedImgIndex]);
-
   return (
     <>
       <StyledPhotoCarousel data-label="photo-carousel">
@@ -67,7 +58,6 @@ export default function PhotoCarousel({ imgUrls }: Props) {
         <div {...swipeHandlers}>
           <StyledCarouselPhoto
             key={`selected-photo-${selectedImgIndex}`}
-            ref={selectedPhotoRef}
             id="photo"
             imgUrl={`url(${imgUrls[selectedImgIndex]})`}
             data-label="carousel-photo"
