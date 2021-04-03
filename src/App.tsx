b03120286db4217e8e7b1477b3aa6f98d1871598
Kim <kim.bertrand@live.com>
2021-04-03 18:11:31 +0200

    created firebaseContext

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,20 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import RecipeOverviewPage from './pages/recipe-overview-page/recipe-overview-page';
 import RecipeSummaryPage from './pages/recipe-summary-page/recipe-summary-page';
 import AppMenu from './components/app-menu/app-menu';
+import { FirebaseProvider } from './contexts/firebase-auth-context';
 
 function App() {
   return (
-    <Router>
-      <AppMenu />
-      <Switch>
-        <Route exact path="/" component={RecipeOverviewPage} />
-        <Route path="/summary/:recipeId" component={RecipeSummaryPage} />
-        {/*<Route path="/add/:recipeId" component={RecipeAddPage} />*/}
-      </Switch>
-    </Router>
+    <FirebaseProvider>
+      <Router>
+        <AppMenu />
+        <Switch>
+          <Route exact path="/" component={RecipeOverviewPage} />
+          <Route path="/summary/:recipeId" component={RecipeSummaryPage} />
+          {/*<Route path="/add/:recipeId" component={RecipeAddPage} />*/}
+        </Switch>
+      </Router>
+    </FirebaseProvider>
   );
 }
 
