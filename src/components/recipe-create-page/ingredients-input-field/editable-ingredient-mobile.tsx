9189d68bc0ce6719803b38d976794d2b538c7bc2
Kim <kim.bertrand@live.com>
2021-04-27 13:56:44 +0200

    Fixed import error

--- src/components/recipe-create-page/ingredients-input-field/editable-ingredient-mobile.tsx
+++ src/components/recipe-create-page/ingredients-input-field/editable-ingredient-mobile.tsx
@@ -1,7 +1,7 @@
 import React, { ReactElement } from 'react';
 import { StyledMobileIngredient } from './ingredients-input-field.styles';
 import type { IIngredient } from '../../../models-and-constants/IRecipe';
-import { StyledRemoveButton } from 'src/components/shared-styles/shared-styles';
+import { StyledRemoveButton } from '../../shared-styles/shared-styles';
 
 type Props = {
   ingredient: IIngredient;
