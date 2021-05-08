3c48d8ad9cff381618a1acda95f4ae27c22dcc04
Kim <kim.bertrand@live.com>
2021-05-08 11:16:46 +0200

    Fix routing issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
-import React, { useContext, useEffect } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import RecipeOverviewPage from './pages/recipe-overview-page/recipe-overview-page';
 import RecipeSummaryPage from './pages/recipe-summary-page/recipe-summary-page';
 import AppMenu from './components/app-menu/app-menu';
-import { FirebaseContext, FirebaseProvider } from './contexts/firebase-auth-context';
+import { FirebaseProvider } from './contexts/firebase-auth-context';
 import { RecipeCreatePage } from './pages/create-recipe-page/recipe-create-page';
 import { useDynamicPageTitle } from './hooks/use-dynamic-page-title';
 
-function App() {
+export default function App() {
   useDynamicPageTitle();
-  const firebaseContext = useContext(FirebaseContext);
-  const isLoggedIn = firebaseContext.user;
 
   return (
     <FirebaseProvider>
@@ -19,17 +17,15 @@ function App() {
         <Switch>
           <Route exact path="/" component={RecipeOverviewPage} />
           <Route path="/summary/:recipeId" component={RecipeSummaryPage} />
-          {isLoggedIn && <Route path="/create" component={RecipeCreatePage} /> }
+          <Route path="/create" component={RecipeCreatePage} />
           {/*<Route path="/edit/:recipeId" component={EditRecipePage} />*/}
           {/*<Route path="/my-recipes" component={MyRecipesPage} />*/}
           {/*<Route path="/my-profile" component={MyProfilePage} />*/}
-          <Route path='*'>
-            <Redirect to='/'/>
+          <Route path="*">
+            <Redirect to="/" />
           </Route>
         </Switch>
       </Router>
     </FirebaseProvider>
   );
 }
-
-export default App;
--- src/pages/create-recipe-page/recipe-create-page.tsx
+++ src/pages/create-recipe-page/recipe-create-page.tsx
@@ -1,10 +1,16 @@
 import { StyledSingleRecipePage } from '../recipe-summary-page/recipe-summary-page.styles';
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import { RecipeNameInputField } from '../../components/recipe-create-page/recipe-name-input-field/recipe-name-input-field';
 import { IngredientsInputCard } from '../../components/recipe-create-page/ingredients-input-card/ingredients-input-card';
+import { FirebaseContext } from '../../contexts/firebase-auth-context';
+import { Redirect } from 'react-router-dom';
 
 export function RecipeCreatePage() {
   const [recipeNameHasError, setRecipeNameHasError] = useState<boolean>();
+  const firebaseContext = useContext(FirebaseContext);
+  const isLoggedIn = firebaseContext.user;
+
+  if (!isLoggedIn) return <Redirect to="" />;
 
   return (
     <StyledSingleRecipePage data-label="create-recipe-page">
