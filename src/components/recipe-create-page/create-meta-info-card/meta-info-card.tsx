54624e043b1d1aa11c031d8b0a798cd5d0a96e66
Kim <kim.bertrand@live.com>
2021-06-12 19:06:17 +0200

    Added meta info block

--- src/components/recipe-create-page/create-meta-info-card/meta-info-card.tsx
+++ src/components/recipe-create-page/create-meta-info-card/meta-info-card.tsx
@@ -1,15 +1,38 @@
 import React from 'react';
-import { StyledBody, StyledSummaryCard, StyledTitle } from '../../shared-styles/shared-styles';
+import { StyledBody, StyledNumericField, StyledSummaryCard, StyledTitle } from '../../shared-styles/shared-styles';
 import { ActiveLabelDictionary } from '../../recipe-label/recipe-labels';
 import { allLabels } from '../../recipe-label/label-constants';
-import { StyledLabelBlock } from './meta-info-card.styles';
+import {
+  StyledDetailsBlock,
+  StyledDetailsItem,
+  StyledDetailsItemLabel,
+  StyledLabelBlock,
+  StyledLabelList,
+} from './meta-info-card.styles';
 
 export function MetaInfoCard() {
   return (
     <StyledSummaryCard>
       <StyledTitle data-label="title">Recipe details</StyledTitle>
       <StyledBody>
-        <StyledLabelBlock>{allLabels.map((l) => ActiveLabelDictionary.get(l.value))}</StyledLabelBlock>
+        <StyledDetailsBlock>
+          <StyledDetailsItem>
+            <StyledDetailsItemLabel>Portions* </StyledDetailsItemLabel>
+            <StyledNumericField />
+          </StyledDetailsItem>
+          <StyledDetailsItem>
+            <StyledDetailsItemLabel>Cooking time* </StyledDetailsItemLabel>
+            <StyledNumericField placeholder="minutes" />
+          </StyledDetailsItem>
+          <StyledDetailsItem>
+            <StyledDetailsItemLabel>Kcal* </StyledDetailsItemLabel>
+            <StyledNumericField />
+          </StyledDetailsItem>
+        </StyledDetailsBlock>
+        <StyledLabelBlock>
+          <StyledDetailsItemLabel>Labels </StyledDetailsItemLabel>
+          <StyledLabelList>{allLabels.map((l) => ActiveLabelDictionary.get(l.value))}</StyledLabelList>
+        </StyledLabelBlock>
       </StyledBody>
     </StyledSummaryCard>
   );
--- src/components/recipe-label/recipe-label.styles.ts
+++ src/components/recipe-label/recipe-label.styles.ts
@@ -12,9 +12,11 @@ export const StyledRecipeLabel = styled.div<LabelProps>`
   padding: 0.25rem 0.5rem;
   font-size: 1rem;
   display: flex;
+  width: 6rem;
   align-items: center;
   justify-content: center;
   cursor: ${(props) => (props.clickAble === 'true' ? 'pointer' : 'auto')};
+  margin: 0.5rem 0;
 
   @media (min-width: 500px) {
     width: 8rem;
