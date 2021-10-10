bfe32dd31e4bcda342b42a2f0d195f0916903593
Kim <kim.bertrand@live.com>
2021-10-10 14:05:53 +0200

    Fix decimal nr bug

--- src/components/recipe-summary-page/recipe-ingredients-block/ingredient.tsx
+++ src/components/recipe-summary-page/recipe-ingredients-block/ingredient.tsx
@@ -12,12 +12,17 @@ type Props = {
 export function Ingredient({ ingredient }: Props): React.ReactElement {
   const [isChecked, setIsChecked] = useState<boolean>();
 
+  function formatNumber(nr: number): string {
+    if (nr % 10 === 0) return nr.toString();
+    return nr.toFixed(1);
+  }
+
   return (
     <StyledIngredient data-label="ingredient" onClick={() => setIsChecked(!isChecked)}>
       <FontAwesomeIcon icon={isChecked ? faCheckSquare : faSquare} />
       <p style={{ display: 'grid', gridTemplateColumns: '1fr 5fr' }}>
         <span style={{ textDecoration: isChecked ? 'line-through' : 'none', minWidth: '3.5rem' }} id="amount">
-          {ingredient.amount} {ingredient.measurement}
+          {formatNumber(ingredient.amount ?? 0)} {ingredient.measurement}
         </span>
         <span style={{ textDecoration: isChecked ? 'line-through' : 'none' }} id="product">
           {' '}
